2d1c15c7849b0e12a706669e6fb3327460639a17
jGabrielrc98 <jgabrielrc7@gmail.com>
2024-11-20 18:28:22 -0300

    SecçãoOne
    Alteração no e na secção one, coloquei o video ao inves do carrosel, onde tirei o saiba mais e o veja mais, deixando mais minimalista, tive que alterar e colocar validações para o google chrome aceita o video, colcoando no mudo e vazendo uma validação que caso não rode automaticamente ele permita ao usuario aperta o play

--- lib/componets/section_one.dart
+++ lib/componets/section_one.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:async'; // Importando o Timer
+import 'package:video_player/video_player.dart';
 
 class SectionOne extends StatefulWidget {
   const SectionOne({super.key});
@@ -9,173 +9,90 @@ class SectionOne extends StatefulWidget {
 }
 
 class _SectionOneState extends State<SectionOne> {
-  // Lista de imagens para o carrossel
-  final List<String> _images = [
-    'lib/assets/images/imagemInicio.jpeg',
-    'lib/assets/images/ImagemOne.jpeg',
-    'lib/assets/images/ImagemTwo.jpeg',
-    'lib/assets/images/ImagemThree.jpeg',
-    'lib/assets/images/ImagemFour.jpeg',
-    'lib/assets/images/ImagemFive.jpeg',
-    'lib/assets/images/ImagemSix.jpeg',
-    'lib/assets/images/ImagemSeven.jpeg',
-  ];
-
-  // Índice da imagem atual sendo exibida
-  int _currentIndex = 0;
-
-  // Controlador do Timer
-  late Timer _timer;
+  final String _videoPath = 'lib/assets/videos/VideoDeInicio.mp4';
+  late VideoPlayerController _controller;
+  bool _isAutoplayFailed = false;
 
   @override
   void initState() {
     super.initState();
-    // Inicia o Timer para a troca automática de imagens
-    _startAutoSlide();
+    _initializeVideoPlayer();
   }
 
   @override
   void dispose() {
-    _timer.cancel(); // Cancela o Timer quando o widget for destruído
+    _controller.dispose();
     super.dispose();
   }
 
-  // Função que inicia o Timer
-  void _startAutoSlide() {
-    _timer = Timer.periodic(Duration(seconds: 3), (timer) {
-      _nextImage(); // Troca a imagem a cada 3 segundos
+  void _initializeVideoPlayer() {
+    _controller = VideoPlayerController.asset(_videoPath)
+      ..initialize().then((_) {
+        setState(() {});
+        _controller.setLooping(true);
+        _controller.setVolume(0.0); // Remove o som para evitar bloqueios
+        _attemptPlay();
+      }).catchError((error) {
+        print('Erro ao inicializar o vídeo: $error');
+      });
+  }
+
+  void _attemptPlay() {
+    _controller.play().catchError((error) {
+      print('Erro ao tentar reproduzir automaticamente: $error');
+      setState(() {
+        _isAutoplayFailed = true;
+      });
     });
   }
 
-  // Função para avançar para a próxima imagem
-  void _nextImage() {
+  void _playVideo() {
+    _controller.play();
     setState(() {
-      if (_currentIndex < _images.length - 1) {
-        _currentIndex++;
-      } else {
-        _currentIndex =
-            0; // Volta para a primeira imagem quando chegar no final
-      }
-    });
-  }
-
-  // Função para voltar para a imagem anterior
-  void _prevImage() {
-    setState(() {
-      if (_currentIndex > 0) {
-        _currentIndex--;
-      } else {
-        _currentIndex = _images.length -
-            1; // Vai para a última imagem se já estiver na primeira
-      }
+      _isAutoplayFailed = false;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 700,
-      color: Colors.black,
-      child: Stack(
-        children: [
-          // Exibe a imagem atual do carrossel
-          AnimatedSwitcher(
-            duration:
-                Duration(milliseconds: 500), // Duração da animação de transição
-            child: Image.asset(
-              _images[_currentIndex],
-              key: ValueKey<int>(
-                  _currentIndex), // Chave única para cada imagem, necessária para o AnimatedSwitcher
-              height: 500,
-              width: double.infinity,
-              fit: BoxFit.cover,
-            ),
-          ),
-          Positioned(
-            left: 20,
-            top: 50,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'EVERTON SILVA',
-                  style: TextStyle(
-                      color: Color.fromARGB(255, 1, 9, 103),
-                      fontSize: 36,
-                      fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  'CONSTRUTORA',
-                  style: TextStyle(
-                      color: Color.fromARGB(255, 1, 9, 103),
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 20),
-                Row(
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                        'VEJA MAIS',
-                        style: TextStyle(color: Colors.black),
+      child: Container(
+        width: MediaQuery.of(context).size.width, // Largura total da tela
+        height: 600, // Altura fixa
+        color: Colors.black,
+        child: Stack(
+          children: [
+            // Exibe o vídeo se ele foi inicializado
+            _controller.value.isInitialized
+                ? Positioned.fill(
+                    child: FittedBox(
+                      fit: BoxFit.fill, // O vídeo preencherá o espaço definido
+                      child: SizedBox(
+                        width: _controller.value.size.width,
+                        height: _controller.value.size.height,
+                        child: VideoPlayer(_controller),
                       ),
                     ),
-                    SizedBox(width: 10),
-                    ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Color.fromARGB(255, 87, 124, 255)),
-                      child: Text(
-                        'SOBRE NÓS',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                    ),
-                  ],
+                  )
+                : Center(
+                    child:
+                        CircularProgressIndicator()), // Indicador de carregamento
+            // Botão para reprodução manual, caso o autoplay falhe
+            if (_isAutoplayFailed)
+              Positioned(
+                bottom: 50,
+                left: 50,
+                right: 50,
+                child: Center(
+                  child: ElevatedButton(
+                    onPressed: _playVideo,
+                    child: Text('Clique para reproduzir'),
+                  ),
                 ),
-              ],
-            ),
-          ),
-          // Botões de navegação (setas)
-          Positioned(
-            left: 10,
-            top: 250, // Ajuste a posição conforme necessário
-            child: IconButton(
-              icon: Icon(Icons.arrow_back_ios, color: Colors.white, size: 40),
-              onPressed: _prevImage,
-            ),
-          ),
-          Positioned(
-            right: 10,
-            top: 250, // Ajuste a posição conforme necessário
-            child: IconButton(
-              icon:
-                  Icon(Icons.arrow_forward_ios, color: Colors.white, size: 40),
-              onPressed: _nextImage,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
+              ),
+          ],
         ),
-        body: const SectionOne(), // Chama a SectionOne dentro do body
       ),
     );
   }
-}
-
-void main() {
-  runApp(const MyApp());
 }
